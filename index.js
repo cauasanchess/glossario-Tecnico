40aefaef72037697d6aafed71103842279cbfd45
CAUÂ SANCHES DA COSTA NEGRÃO <caua_negrao@estudante.sesisenai.org.br>
2025-08-15 12:30:52 -0300

    subindo projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const accordionContent = header.nextElementSibling;
+        accordionContent.classList.toggle('active');
+    });
+});
+
